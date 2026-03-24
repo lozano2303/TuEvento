760b30371533f6fddf6a19be8523f742b830e967
Cristofer Lozano <cristoferlozano233@gmail.com>
2026-03-24 16:53:23 -0500

    chore: add DDD modular structure and update README with project layout

---
+++ TuEventoBackend/tu-evento/src/main/java/com/capysoft/tuevento/TuEventoApplication.java
@@ -0,0 +1,13 @@
+package com.capysoft.tuevento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuEventoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuEventoApplication.class, args);
+	}
+
+}
---
+++ TuEventoBackend/tu-evento/src/test/java/com/capysoft/tuevento/TestTuEventoApplication.java
@@ -0,0 +1,11 @@
+package com.capysoft.tuevento;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestTuEventoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(TuEventoApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
