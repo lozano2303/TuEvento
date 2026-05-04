74fc2b868a1a16e3b258a233726c210c63998f3a
Jhampier <ortizjhampier@gmail.com>
2026-05-04 09:12:39 -0500

    Add log-based wait strategy for PostgreSQL to fix Docker-in-Docker timeout

--- TuEventoBackend/tu-evento/src/test/java/com/capysoft/tuevento/TestcontainersConfiguration.java
+++ TuEventoBackend/tu-evento/src/test/java/com/capysoft/tuevento/TestcontainersConfiguration.java
@@ -21,7 +21,8 @@ class TestcontainersConfiguration {
 	@Bean
 	@ServiceConnection
 	PostgreSQLContainer<?> postgresContainer() {
-		return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+		return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"))
+				.waitingFor(Wait.forLogMessage(".*database system is ready to accept connections.*", 1));
 	}
 
 	@Bean
